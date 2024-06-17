c2d1bb5df0fc48c1f33918fdce073f4f238bd0e6
Anum Qudsia <hello@anum.io>
2024-06-17 15:32:04 +0200

    Allow incremental adoption for ppr. Dynamically load TokenList. Statically load the rest in page.tsx

---
+++ app/components/loading.tsx
@@ -0,0 +1,36 @@
+export default function Loading() {
+  return (
+    <div className="px-10">
+      <table className="w-full">
+        <thead>
+          <tr className="w-full border-b-2 h-20 border-slate-700">
+            <th className="w-4/12">Name</th>
+            <th className="w-6/12">Address</th>
+            <th className="w-2/12">Logo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(Array(30).keys()).map((token) => (
+            <tr key={token} className="border-b-2 border-gray-800 h-20">
+              <td className="">
+                <div className="animate-pulse bg-white-200 w-80 mx-auto">
+                  <div className="h-10 bg-slate-700 rounded"></div>
+                </div>
+              </td>
+              <td className="">
+                <div className="animate-pulse bg-white-200 w-9/12 mx-auto">
+                  <div className="h-10 bg-slate-700 rounded"></div>
+                </div>
+              </td>
+              <td className="justify-center h-20 items-center">
+                <div className="animate-pulse bg-white-200 mx-auto w-2/12">
+                  <div className="h-12 bg-slate-700 rounded"></div>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { Suspense } from "react";
 import TokenList from "./components/TokenList";
+import Loading from "./components/loading";
+
+export const experimental_ppr = true;
 
 export default function Home() {
   return (
@@ -10,7 +13,9 @@ export default function Home() {
         <div className="flex justify-center items-center py-10">
           <h1 className="text-5xl">Overview</h1>
         </div>
-        <TokenList search={""} />
+        <Suspense fallback={<Loading />}>
+          <TokenList search={""} />
+        </Suspense>
       </main>
       <footer></footer>
     </div>
